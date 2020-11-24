b10f03446bcfd17b7cae8dfb73cd152d9e286b0c
Murshidmac <mh.murshidco.uk@outlook.com>
2020-11-25 00:01:57 +0530

    test api added

--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from pycricbuzz import Cricbuzz
+import json
 from .models import Greeting
+
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
 
+def cricket(request):
+    c = Cricbuzz();
+    matches = c.matches();
+    return render(request, json.dumps(matches,indent=4));
 
 def db(request):
 
